116682e039ff29daad958e6d2adb5bcb5d36b187
Yani <84055958+Yanina-31@users.noreply.github.com>
2021-11-03 13:18:32 -0300

    Ejercicio 3 finalizado

---
+++ Ejercicio_3/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 1000;
+        String s = Integer.toBinaryString(n);
+        System.out.println(s);
+
+    }
+
+}
